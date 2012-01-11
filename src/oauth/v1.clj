aa471822572447a2b079b0c5b1030c9602231361
Roman <roman@burningswell.com>
2012-01-11 22:50:03 +0100

    Added percent-encode.

--- src/oauth/v1.clj
+++ src/oauth/v1.clj
@@ -1,7 +1,8 @@
 (ns oauth.v1
+  (:refer-clojure :exclude (replace))
   (:require [clj-http.client :as http])
-  (:use [clj-http.util :only (url-encode)]
-        [clojure.string :only (join upper-case)]
+  (:use [clj-http.util :only (url-encode url-decode)]
+        [clojure.string :only (join replace split upper-case)]
         [inflections.core :only (underscore)]))
 
 (def ^:dynamic *oauth-consumer-key* nil)
@@ -15,6 +16,14 @@
 
 (defn format-header [options]
   (str "OAuth " (join ", " (format-options options))))
+
+(defn percent-encode
+  "Percent encode `unencoded` according to RFC 3986, Section 2.1."
+  [unencoded]
+  (-> (url-encode unencoded)
+      (replace "%7E" "~")
+      (replace "*" "%2A")
+      (replace "+" "%20")))
 
 (defn root-url [{:keys [scheme server-name server-port]}]
   (str scheme "://" server-name (when server-port (str ":" server-port))))
@@ -33,3 +42,27 @@
 
 (defn oauth-request-signature [request]
   (oauth-signature-base-string request))
+
+(defn oauth-parameters [request]
+  (let [body (split (:body request) #"=")
+        params  (reduce #(concat %1 [(name (first %2)) (str (last %2))]) nil (:query-params request))]
+    (assoc (apply sorted-map (map url-decode (concat body params)))
+      "oauth_consumer_key" (:oauth-consumer-key request)
+      "oauth_nonce" (:oauth-nonce request)
+      "oauth_signature_method" (:oauth-signature-method request)
+      "oauth_timestamp" (:oauth-timestamp request)
+      "oauth_token" (:oauth-token request)
+      "oauth_version" (:oauth-version request))))
+
+(defn oauth-parameter-string [request]
+  (->> (oauth-parameters request)
+       (map #(str (percent-encode (first %1)) "=" (percent-encode (last %1))))
+       (join "&")))
+
+;; (prn (oauth-parameters
+;;       {:method :post
+;;        :scheme "https"
+;;        :server-name "api.twitter.com"
+;;        :uri "/1/statuses/update.json"
+;;        :query-params {:include_entities true}
+;;        :body "status=Hello%20Ladies%20%2b%20Gentlemen%2c%20a%20signed%20OAuth%20request%21"}))
--- test/oauth/test/v1.clj
+++ test/oauth/test/v1.clj
@@ -20,6 +20,9 @@
    :query-params {:include_entities true}
    :body "status=Hello%20Ladies%20%2b%20Gentlemen%2c%20a%20signed%20OAuth%20request%21"})
 
+(def example-oauth-request
+  (merge example-request example-options))
+
 (deftest test-format-base-url
   (are [request expected]
     (is (= expected (format-base-url request)))
@@ -44,8 +47,41 @@
           "oauth_consumer_key=\"xvz1evFS4wEEPTGEFPHBog\"")
      (format-options example-options)))
 
+(deftest test-percent-encode
+  (are [unencoded expected]
+    (is (= expected (percent-encode unencoded)))
+    "" ""
+    "Ladies + Gentlemen" "Ladies%20%2B%20Gentlemen"
+    "An encoded string!" "An%20encoded%20string%21"
+    "Dogs, Cats & Mice" "Dogs%2C%20Cats%20%26%20Mice"
+    "☃" "%E2%98%83")) ; https://dev.twitter.com/docs/auth/percent-encoding-parameters
+
 (deftest test-root-url
   (is (= "https://api.twitter.com" (root-url example-request))))
+
+(deftest test-oauth-parameter-string
+  (is (= (str "include_entities=true&"
+              "oauth_consumer_key=xvz1evFS4wEEPTGEFPHBog&"
+              "oauth_nonce=kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg&"
+              "oauth_signature_method=HMAC-SHA1&"
+              "oauth_timestamp=1318622958&"
+              "oauth_token=370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb&"
+              "oauth_version=1.0&"
+              "status=Hello%20Ladies%20%2B%20Gentlemen%2C%20a%20signed%20OAuth%20request%21")
+         (oauth-parameter-string example-oauth-request))))
+
+(deftest test-oauth-parameters
+  (is (map? (oauth-parameters example-oauth-request)))
+  (let [params (seq (oauth-parameters example-oauth-request))]
+    (is (= 8 (count params)))
+    (is (= ["include_entities" "true"] (nth params 0)))
+    (is (= ["oauth_consumer_key" "xvz1evFS4wEEPTGEFPHBog"] (nth params 1)))
+    (is (= ["oauth_nonce" "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg"] (nth params 2)))
+    (is (= ["oauth_signature_method" "HMAC-SHA1"] (nth params 3)))
+    (is (= ["oauth_timestamp" "1318622958"] (nth params 4)))
+    (is (= ["oauth_token" "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb"] (nth params 5)))
+    (is (= ["oauth_version" "1.0"] (nth params 6)))
+    (is (= ["status" "Hello Ladies + Gentlemen, a signed OAuth request!"] (nth params 7)))))
 
 (deftest test-oauth-request-signature
   (let [request (oauth-request-signature example-request)]
