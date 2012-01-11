22464fd368c69490c3dffc4c0da148f12dd405cc
Roman <roman@burningswell.com>
2012-01-11 20:28:14 +0100

    Initial import.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject oauth2-clj "0.0.1-SNAPSHOT"
+  :description "A OAuth library for Clojure."
+  :dependencies [[clj-http "0.2.6"]
+                 [inflections "0.6.5-SNAPSHOT"]
+                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.1.2"]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [org.slf4j/slf4j-log4j12 "1.5.6"]
+                 [slingshot "0.10.0"]])
