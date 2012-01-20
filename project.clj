ef6f91dae392832fd0e16de510feabcc49a7a907
Roman <roman@burningswell.com>
2012-01-20 17:27:11 +0100

    Removed slingshot and clojure.data.json.

--- project.clj
+++ project.clj
@@ -3,7 +3,5 @@
   :dependencies [[clj-http "0.2.6"]
                  [inflections "0.6.5-SNAPSHOT"]
                  [org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.2"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [org.slf4j/slf4j-log4j12 "1.5.6"]
-                 [slingshot "0.10.0"]])
+                 [org.slf4j/slf4j-log4j12 "1.5.6"]])
