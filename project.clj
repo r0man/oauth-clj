e684b198217a3b28a91c604a291c6396d703f58b
Roman <roman@burningswell.com>
2012-01-20 18:04:13 +0100

    Switched to clj-http 0.2.7.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject oauth-clj "0.0.1-SNAPSHOT"
   :description "A OAuth library for Clojure."
-  :dependencies [[clj-http "0.2.6"]
+  :dependencies [[clj-http "0.2.7"]
                  [inflections "0.6.5-SNAPSHOT"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
