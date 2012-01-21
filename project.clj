5e255c02a16abcf7bb71230cab8196f5b7b725ed
Roman <roman@burningswell.com>
2012-01-21 23:59:14 +0100

    Changed description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject oauth-clj "0.0.1-SNAPSHOT"
-  :description "A OAuth library for Clojure."
+  :description "A Clojure OAuth library."
   :dependencies [[clj-http "0.2.7"]
                  [inflections "0.6.5-SNAPSHOT"]
                  [org.clojure/clojure "1.3.0"]
