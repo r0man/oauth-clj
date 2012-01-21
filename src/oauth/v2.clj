64e777961540ee0bda57055fffc41c463ef3b8e5
Roman <roman@burningswell.com>
2012-01-21 18:49:38 +0100

    Added oauth-authorize.

--- src/oauth/v2.clj
+++ src/oauth/v2.clj
@@ -1,6 +1,7 @@
 (ns oauth.v2
   (:require [clj-http.client :as http])
-  (:use [clojure.string :only (blank? join)]
+  (:use [clojure.java.browse :only (browse-url)]
+        [clojure.string :only (blank? join)]
         [inflections.core :only (hyphenize underscore)]
         [inflections.transform :only (transform-keys)]
         oauth.util))
@@ -9,7 +10,14 @@
   "Returns the OAuth authorization url."
   [url client-id redirect-uri & {:as options}]
   (let [params
-        (-> (assoc options :redirect-uri redirect-uri)
+        (-> (assoc options
+              :client-id client-id
+              :redirect-uri redirect-uri)
             (transform-keys (comp name underscore))
             (http/generate-query-string))]
     (if (blank? params) url (str url "?" params))))
+
+(defn oauth-authorize
+  "Send the user to the authorization url via `browse-url`."
+  [url client-id redirect-uri & options]
+  (browse-url (apply oauth-authorization-url url client-id redirect-uri options)))
--- test/oauth/test/v2.clj
+++ test/oauth/test/v2.clj
@@ -1,11 +1,16 @@
 (ns oauth.test.v2
   (:require [clj-http.client :as http])
-  (:use clojure.test
+  (:use [clojure.java.browse :only (browse-url)]
+        clojure.test
         oauth.test.examples
         oauth.v2))
 
 (deftest test-oauth-authorization-url
-  (is (= "https://www.facebook.com/dialog/oauth?redirect_uri=http%3A%2F%2Fexample.com"
+  (is (= "https://www.facebook.com/dialog/oauth?client_id=287169314674516&redirect_uri=http%3A%2F%2Fexample.com"
          (oauth-authorization-url "https://www.facebook.com/dialog/oauth" "287169314674516" "http://example.com")))
-  (is (= "https://www.facebook.com/dialog/oauth?redirect_uri=http%3A%2F%2Fexample.com&scope=email%2Cread_stream"
+  (is (= "https://www.facebook.com/dialog/oauth?redirect_uri=http%3A%2F%2Fexample.com&client_id=287169314674516&scope=email%2Cread_stream"
          (oauth-authorization-url "https://www.facebook.com/dialog/oauth" "287169314674516" "http://example.com" :scope "email,read_stream"))))
+
+(deftest test-oauth-authorize
+  (with-redefs [browse-url (fn [url] (is (= "https://www.facebook.com/dialog/oauth?client_id=287169314674516&redirect_uri=http%3A%2F%2Fexample.com" url)))]
+    (oauth-authorize "https://www.facebook.com/dialog/oauth" "287169314674516" "http://example.com")))
