7d7d59cdf307c23177bd1fda3f792b561452b3dd
Roman <roman@burningswell.com>
2012-01-22 00:38:45 +0100

    Release 0.0.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oauth-clj "0.0.1-SNAPSHOT"
+(defproject oauth-clj "0.0.1"
   :description "Clojure OAuth library."
   :dependencies [[clj-http "0.2.7"]
                  [inflections "0.6.5-SNAPSHOT"]
