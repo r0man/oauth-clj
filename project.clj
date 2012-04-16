a51f21bcd011e80bb05259d4e58b311c328a0356
Roman <roman@burningswell.com>
2012-04-16 20:10:55 +0200

    Switched to Clojure 1.4.0.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Clojure OAuth library."
   :dependencies [[clj-http "0.2.7"]
                  [inflections "0.6.5"]
-                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.slf4j/slf4j-log4j12 "1.5.6"]]
