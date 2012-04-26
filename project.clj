3d18f4d4ad89c9835d57f9d0cfdbc7753bf00859
Roman <roman@burningswell.com>
2012-04-26 21:24:03 +0200

    Switched to clj-http "0.4.0".

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject oauth-clj/oauth-clj "0.0.5-SNAPSHOT"
   :description "Clojure OAuth library."
-  :dependencies [[clj-http "0.2.7"]
+  :min-lein-version "2.0.0"
+  :dependencies [[clj-http "0.4.0"]
                  [inflections "0.6.5"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.slf4j/slf4j-log4j12 "1.5.6"]]
-  :profiles {:dev {:resource-paths ["test-resources"]}}
-  :min-lein-version "2.0.0")
+  :profiles {:dev {:resource-paths ["test-resources"]}})
