743798efa97df47f395066b9d25c22d5aa945180
Roman Scherer <roman.scherer@burningswell.com>
2012-10-21 15:08:27 +0200

    Release 0.0.9.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oauth-clj/oauth-clj "0.0.9-SNAPSHOT"
+(defproject oauth-clj/oauth-clj "0.0.9"
   :description "Clojure OAuth library."
   :min-lein-version "2.0.0"
   :dependencies [[clj-http "0.5.6"]
