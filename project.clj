1a4a3f4594dab55480d317d5f5f33a54dfa80dbf
Roman Scherer <roman.scherer@burningswell.com>
2012-12-28 20:37:31 +0100

    Switched to inflections 0.7.4.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Clojure OAuth library."
   :min-lein-version "2.0.0"
   :dependencies [[clj-http "0.5.6"]
-                 [inflections "0.7.3"]
+                 [inflections "0.7.4"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.2.0"]]
   :profiles {:dev {:dependencies [[org.slf4j/slf4j-log4j12 "1.6.6"]]
