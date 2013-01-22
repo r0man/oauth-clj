1fa64eed74456ce414becd780ce24d3543808583
Roman Scherer <roman.scherer@burningswell.com>
2013-01-22 21:10:29 +0100

    Switched to org.clojure/data.json 0.2.1.

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
   :dependencies [[clj-http "0.6.3"]
                  [inflections "0.7.5-SNAPSHOT"]
                  [org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.json "0.2.0"]]
+                 [org.clojure/data.json "0.2.1"]]
   :profiles {:dev {:dependencies [[org.slf4j/slf4j-log4j12 "1.6.6"]]
                    :resource-paths ["test-resources"]}})
