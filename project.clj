5b3e8a1954c3b08bd498af1952511c4012a3a7ed
Roman Scherer <roman.scherer@burningswell.com>
2013-03-25 19:47:57 +0100

    Switched to clj-http 0.7.0.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-http "0.6.5"]
+  :dependencies [[clj-http "0.7.0"]
                  [inflections "0.8.0"]
                  [org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[org.slf4j/slf4j-log4j12 "1.6.6"]]
