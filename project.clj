2c158189a691338eb263a70b1ca76039850bc0d5
Roman Scherer <roman.scherer@burningswell.com>
2013-08-30 15:31:41 +0200

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-http "0.7.0"]
-                 [inflections "0.8.0"]
+  :dependencies [[clj-http "0.7.6"]
+                 [inflections "0.8.2"]
                  [org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[org.slf4j/slf4j-log4j12 "1.6.6"]]
+  :profiles {:dev {:dependencies [[org.slf4j/slf4j-log4j12 "1.7.5"]]
                    :resource-paths ["test-resources"]}})
