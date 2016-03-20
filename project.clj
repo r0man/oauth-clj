f60b6d9f35d1d626fe861f7c5ddbed5a6b596958
Roman Scherer <roman@burningswell.com>
2016-03-20 14:35:47 +0100

    Version 0.1.16-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject oauth-clj "0.1.15"
+(defproject oauth-clj "0.1.16-SNAPSHOT"
   :description "Clojure OAuth library."
   :url "https://github.com/r0man/oauth-clj"
   :min-lein-version "2.0.0"
